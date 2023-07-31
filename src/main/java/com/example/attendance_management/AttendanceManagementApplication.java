3fbda9c0328d145e15cb3420f4d2b51fbd78c6e1
tare2 <tare2701@gmail.com>
2023-07-31 13:57:26 +0200

    Initial commit

---
+++ src/main/java/com/example/attendance_management/AttendanceManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.attendance_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendanceManagementApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Colleagues");
+	}
+
+}
